8d537472f8930385c505bff324eea5638cb7227c
Barneypoi <beiren.hi@163.com>
2019-06-27 15:10:05 +0800

    去除标题栏，实现返回主界面

--- app/src/main/java/com/example/myapplication/FilterActivity.java
+++ app/src/main/java/com/example/myapplication/FilterActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class FilterActivity extends AppCompatActivity {
     @Override
@@ -9,4 +13,11 @@ public class FilterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filter);
     }
+
+    public void backToMain(View view){
+        Intent intent= new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
 }
