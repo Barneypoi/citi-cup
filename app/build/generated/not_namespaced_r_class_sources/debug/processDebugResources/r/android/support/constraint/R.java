b2b2e114024f167529ec29ef4e6b185ea15f3c17
Barneypoi <beiren.hi@163.com>
2019-06-28 19:58:19 +0800

    首页改成滚动广告

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f080022;
         public static final int end = 0x7f08004f;
         public static final int gone = 0x7f08005c;
-        public static final int invisible = 0x7f080073;
-        public static final int left = 0x7f080078;
-        public static final int packed = 0x7f080090;
-        public static final int parent = 0x7f080092;
-        public static final int percent = 0x7f080095;
-        public static final int right = 0x7f08009a;
-        public static final int spread = 0x7f0800c3;
-        public static final int spread_inside = 0x7f0800c4;
-        public static final int start = 0x7f0800c9;
-        public static final int top = 0x7f0800f1;
-        public static final int wrap = 0x7f080107;
+        public static final int invisible = 0x7f080070;
+        public static final int left = 0x7f080075;
+        public static final int packed = 0x7f08008d;
+        public static final int parent = 0x7f08008f;
+        public static final int percent = 0x7f080092;
+        public static final int right = 0x7f080097;
+        public static final int spread = 0x7f0800c0;
+        public static final int spread_inside = 0x7f0800c1;
+        public static final int start = 0x7f0800c6;
+        public static final int top = 0x7f0800ee;
+        public static final int wrap = 0x7f080104;
     }
     public static final class styleable {
         private styleable() {}
