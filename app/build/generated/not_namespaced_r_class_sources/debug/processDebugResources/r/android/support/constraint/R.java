8ef3ed4f91216a037ba984722547f22d28828cfa
Barneypoi <beiren.hi@163.com>
2019-06-28 09:49:08 +0800

    将筛选选择框换成了更美观的cardview

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f08004b;
-        public static final int gone = 0x7f080058;
-        public static final int invisible = 0x7f08006f;
-        public static final int left = 0x7f080074;
-        public static final int packed = 0x7f080089;
-        public static final int parent = 0x7f08008b;
-        public static final int percent = 0x7f08008e;
-        public static final int right = 0x7f080093;
-        public static final int spread = 0x7f0800ba;
-        public static final int spread_inside = 0x7f0800bb;
-        public static final int start = 0x7f0800c0;
-        public static final int top = 0x7f0800e8;
-        public static final int wrap = 0x7f0800fa;
+        public static final int end = 0x7f08004c;
+        public static final int gone = 0x7f080059;
+        public static final int invisible = 0x7f080070;
+        public static final int left = 0x7f080075;
+        public static final int packed = 0x7f08008a;
+        public static final int parent = 0x7f08008c;
+        public static final int percent = 0x7f08008f;
+        public static final int right = 0x7f080094;
+        public static final int spread = 0x7f0800bc;
+        public static final int spread_inside = 0x7f0800bd;
+        public static final int start = 0x7f0800c2;
+        public static final int top = 0x7f0800eb;
+        public static final int wrap = 0x7f0800fd;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/myapplication/SYFragment.java
+++ app/src/main/java/com/example/myapplication/SYFragment.java
@@ -12,7 +12,6 @@ public class SYFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab1, container, false);
-        
         return view;
     }
 }
