6dc2bebbf54a3903c89dbb53f2c61f99c20d56b0
Jiaqing <1051302489@qq.com>
2019-06-29 11:13:17 +0800

    merge

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/compat/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/compat/R.java
@@ -28,10 +28,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f050072;
-        public static final int notification_icon_bg_color = 0x7f050073;
-        public static final int ripple_material_light = 0x7f05007d;
-        public static final int secondary_text_default_material_light = 0x7f05007f;
+        public static final int notification_action_color_filter = 0x7f050073;
+        public static final int notification_icon_bg_color = 0x7f050074;
+        public static final int ripple_material_light = 0x7f05007e;
+        public static final int secondary_text_default_material_light = 0x7f050080;
     }
     public static final class dimen {
         private dimen() {}
