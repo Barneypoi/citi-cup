a7f2dd11e465d627e2be0c999489041abbf79279
Jiaqing <1051302489@qq.com>
2019-06-30 19:29:36 +0800

    merge

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -78,15 +78,15 @@ public final class R {
         public static final int gone = 0x7f080062;
         public static final int invisible = 0x7f08007c;
         public static final int left = 0x7f080082;
-        public static final int packed = 0x7f08009e;
-        public static final int parent = 0x7f0800a0;
-        public static final int percent = 0x7f0800a3;
-        public static final int right = 0x7f0800a8;
-        public static final int spread = 0x7f0800d2;
-        public static final int spread_inside = 0x7f0800d3;
-        public static final int start = 0x7f0800d8;
-        public static final int top = 0x7f080106;
-        public static final int wrap = 0x7f08011d;
+        public static final int packed = 0x7f0800a1;
+        public static final int parent = 0x7f0800a3;
+        public static final int percent = 0x7f0800a6;
+        public static final int right = 0x7f0800ab;
+        public static final int spread = 0x7f0800d5;
+        public static final int spread_inside = 0x7f0800d6;
+        public static final int start = 0x7f0800db;
+        public static final int top = 0x7f08010a;
+        public static final int wrap = 0x7f080121;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/swiperefreshlayout/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/swiperefreshlayout/R.java
@@ -28,10 +28,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f050073;
-        public static final int notification_icon_bg_color = 0x7f050074;
-        public static final int ripple_material_light = 0x7f05007e;
-        public static final int secondary_text_default_material_light = 0x7f050080;
+        public static final int notification_action_color_filter = 0x7f050074;
+        public static final int notification_icon_bg_color = 0x7f050075;
+        public static final int ripple_material_light = 0x7f05007f;
+        public static final int secondary_text_default_material_light = 0x7f050081;
     }
     public static final class dimen {
         private dimen() {}
@@ -93,19 +93,19 @@ public final class R {
         public static final int italic = 0x7f08007d;
         public static final int line1 = 0x7f080083;
         public static final int line3 = 0x7f080084;
-        public static final int normal = 0x7f080099;
-        public static final int notification_background = 0x7f08009a;
-        public static final int notification_main_column = 0x7f08009b;
-        public static final int notification_main_column_container = 0x7f08009c;
-        public static final int right_icon = 0x7f0800a9;
-        public static final int right_side = 0x7f0800aa;
-        public static final int tag_transition_group = 0x7f0800de;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800df;
-        public static final int tag_unhandled_key_listeners = 0x7f0800e0;
-        public static final int text = 0x7f0800e1;
-        public static final int text2 = 0x7f0800e2;
-        public static final int time = 0x7f080101;
-        public static final int title = 0x7f080102;
+        public static final int normal = 0x7f08009c;
+        public static final int notification_background = 0x7f08009d;
+        public static final int notification_main_column = 0x7f08009e;
+        public static final int notification_main_column_container = 0x7f08009f;
+        public static final int right_icon = 0x7f0800ac;
+        public static final int right_side = 0x7f0800ad;
+        public static final int tag_transition_group = 0x7f0800e2;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800e3;
+        public static final int tag_unhandled_key_listeners = 0x7f0800e4;
+        public static final int text = 0x7f0800e5;
+        public static final int text2 = 0x7f0800e6;
+        public static final int time = 0x7f080105;
+        public static final int title = 0x7f080106;
     }
     public static final class integer {
         private integer() {}
@@ -115,12 +115,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0b003e;
-        public static final int notification_action_tombstone = 0x7f0b003f;
-        public static final int notification_template_custom_big = 0x7f0b0040;
-        public static final int notification_template_icon_group = 0x7f0b0041;
-        public static final int notification_template_part_chronometer = 0x7f0b0042;
-        public static final int notification_template_part_time = 0x7f0b0043;
+        public static final int notification_action = 0x7f0b003f;
+        public static final int notification_action_tombstone = 0x7f0b0040;
+        public static final int notification_template_custom_big = 0x7f0b0041;
+        public static final int notification_template_icon_group = 0x7f0b0042;
+        public static final int notification_template_part_chronometer = 0x7f0b0043;
+        public static final int notification_template_part_time = 0x7f0b0044;
     }
     public static final class string {
         private string() {}
