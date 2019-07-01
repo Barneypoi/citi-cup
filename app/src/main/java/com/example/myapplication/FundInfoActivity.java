dea6c78bbb1ba9553704a1e9d6386091e76a2a3a
Mashiro <Mashiro@example.com>
2019-07-01 17:23:59 +0800

    基金详情界面净值折线图引入

--- app/src/main/java/com/example/myapplication/FundInfoActivity.java
+++ app/src/main/java/com/example/myapplication/FundInfoActivity.java
@@ -13,8 +13,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class FundInfoActivity extends Activity {
 
@@ -25,7 +33,7 @@ public class FundInfoActivity extends Activity {
 
     private ArrayList<FundInfoObject> fundInfoList = new ArrayList<>();
 
-
+    public ArrayList<Map<String, Object>> mainlist=new ArrayList<Map<String,Object>>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +56,29 @@ public class FundInfoActivity extends Activity {
             }
         });
 
+
+        mainlist = (ArrayList<Map<String, Object>>)getIntent().getExtras().get("key");
+        LineChart mLineChart = (LineChart) findViewById(R.id.lineChart);
+        //显示边界
+        mLineChart.setDrawBorders(true);
+        //设置数据
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 4251; i++) {
+            entries.add(new Entry(i,Float.parseFloat((String) mainlist.get(i).get("po")) ) );
+        }
+        //准备好每个点对应的x轴数值
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 4251; i++) {
+            list.add(TimeStamp2Date((String) mainlist.get(i).get("1")));
+        }
+        XAxis xAxis = mLineChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(list));
+
+        //一个LineDataSet就是一条线
+        LineDataSet lineDataSet;
+        lineDataSet = new LineDataSet(entries, "净值");
+        LineData data = new LineData(lineDataSet);
+        mLineChart.setData(data);
     }
 
     //此函数初始化列表项，将基金信息在此函数中初始化
@@ -67,4 +98,10 @@ public class FundInfoActivity extends Activity {
         fundInfoList.add(fund4);
     }
 
+    public String TimeStamp2Date(String timestampString){
+        Long timestamp = Long.parseLong(timestampString);
+        //String date = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date(timestamp));
+        String date = new java.text.SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date(timestamp));
+        return date;
+    }
 }
