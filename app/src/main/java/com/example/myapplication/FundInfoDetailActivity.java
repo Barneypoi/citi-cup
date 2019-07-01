15ff78f5178be6adeb4c49900bbff40f7bbb7ef3
Mashiro <Mashiro@example.com>
2019-07-01 21:38:10 +0800

    完成折线图，但因需访问服务器，显示等待时间较久

--- app/src/main/java/com/example/myapplication/FundInfoDetailActivity.java
+++ app/src/main/java/com/example/myapplication/FundInfoDetailActivity.java
@@ -42,6 +42,7 @@ public class FundInfoDetailActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_fundinfo_detail);
 
         init();
